9954fed81af42ada107292371ad81762cfd8f6f4
frontend-fuel <neverfail2026@gmail.com>
2025-02-26 16:39:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ import plotly.express as px
 import py3Dmol
 import streamlit.components.v1 as components
 from rdkit import Chem
-from rdkit.Chem import AllChem, Descriptors, Draw
+
 from rdkit.Chem import Descriptors
 from rdkit.Chem.rdMolDescriptors import CalcNumAromaticRings
 import urllib.parse
